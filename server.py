f7a5701ab0febf1a781a9908ac870ea92895890d
Douglas Pontes <jdps@cin.ufpe.br>
2019-06-25 14:08:14 -0300

    tentando implementar threads no servidor

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from socket import *
 import pathlib
 import utils
 from random import *
+import threading
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 12019
@@ -11,7 +12,9 @@ UDP_PORT = 12019
 SERVER_NAME = "www.infracom.com"
 
 #pode ser colocado depois para ser gerado randomicamente
-UDP_PORT_SERVER = 5422
+UDP_PORT_SERVER = 54232
+
+lock = threading.Lock()
 
 def main():
 	#setando meu dominio no DNS
@@ -37,56 +40,72 @@ def main():
 	print("Socket closed")
 
 
+	
+
+	print("Server is on")
 	#criando o socket de comunicação cliente servidor
 	sockd = socket(AF_INET, SOCK_DGRAM) 
 
 	sockd.bind(('', UDP_PORT_SERVER))
 
-	print("Server is on")
-
-	while True:
-		#connectionSocket, addr = sockd.accept()
+	threads = []
+	for i in range(5):
+		t = threading.Thread(target=worker, args=(sockd,))
+		threads.append(t)
+		t.start()
 		
-		messageFromClient, addr = sockd.recvfrom(1024)
-
-		args = messageFromClient.decode().split()
-
-		print("received Option: ",args[0])	
-		
-		if(args[0] == "END"):
-			sockd.sendto("ACK".encode(), addr)
-			break
-
-		elif(args[0] == "LST"):
-
-			sockd.sendto("ACK".encode(), addr)
-			
-			files = ""
-
-			path = pathlib.Path('tests')
-
-			#Verificamos cada arquivo dentro da pasta especificada
-			for currentFile in path.iterdir():  
-				files = files + str(currentFile) + "\n"
-				print(files)
-
-			#utils.sendPKT(socket, files, addr)
-			sockd.sendto(files.encode(),addr)
-
-			#sockd.sendto(files.encode(),addr)
-			print('Lista:',files)
-
-		elif(args[0] == "GET"):
-			
-			sockd.sendto("ACK".encode(), addr)
-			msg = "tests/"+ args[1]
-			utils.sendFile(sockd, msg, addr)
-			print("Returned to main")
-			sockd.setblocking(True)
 
 	print("Farewell")
-	sockd.close()
+	#sockd.close()
+
+
+def worker(sockd):
+	print("chegou aqui")
+	while True:
+	#connectionSocket, addr = sockd.accept()
+		lock.acquire(0)
+		try:
+			messageFromClient, addr = sockd.recvfrom(1024)
+			args = messageFromClient.decode().split()
+
+			print("received Option: ",args[0])	
+			
+			if(args[0] == "END"):
+				sockd.sendto("ACK".encode(), addr)
+				break
+
+			elif(args[0] == "LST"):
+
+				sockd.sendto("ACK".encode(), addr)
+				
+				files = ""
+
+				path = pathlib.Path('tests')
+
+				#Verificamos cada arquivo dentro da pasta especificada
+				for currentFile in path.iterdir():  
+					files = files + str(currentFile) + "\n"
+					print(files)
+
+				#utils.sendPKT(socket, files, addr)
+				sockd.sendto(files.encode(),addr)
+
+				#sockd.sendto(files.encode(),addr)
+				print('Lista:',files)
+
+			elif(args[0] == "GET"):
+				
+				sockd.sendto("ACK".encode(), addr)
+				msg = "tests/"+ args[1]
+				utils.sendFile(sockd, msg, addr)
+				print("Returned to main")
+				sockd.setblocking(True)
+		except:
+			print("so sorry")
+		finally: 
+			lock.release()
+
+		
 
 if __name__ == "__main__":
 	main()
-
